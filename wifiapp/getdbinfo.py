e6fde274a5877e299dbb468fd25fb95d916af4ad
skr-amp <skramp@mail.ru>
2020-10-15 23:39:09 +0500

    added display of information about the access point when clicking on the marker

--- wifiapp/getdbinfo.py
+++ wifiapp/getdbinfo.py
@@ -3,6 +3,11 @@ import pymysql
 from flask import jsonify
 from wifiapp import app
 
+def freqToChannel(freq):
+    chdict = {2412: 1, 2417: 2, 2422: 3, 2427: 4, 2432: 5, 2437: 6, 2442: 7, 2447: 8, 2452: 9, 2457: 10, 2462: 11, 2467: 12, 2472: 13, 2484: 14}
+    return chdict[int(freq)]
+
+
 def apmarkers(bounds):
     boundslist = bounds.split(",")  #
     minlat = float(boundslist[1])  #
@@ -27,5 +32,55 @@ def apmarkers(bounds):
                            "geometry": {"type": "Point", "coordinates": [ap[6], ap[5]]}})
         conn.close()
     res = {"type": "FeatureCollection", "features": aplist}
-    print(res)
+    return jsonify(res)
+
+
+def apinfo(apid):
+    if app.config['CURRENT_DB_TYPE'] == 'sqlite':
+        conn = sqlite3.connect('wifiapp/localdb/' + app.config['CURRENT_DB_NAME'])
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM ap WHERE id=?', (apid,))
+        apinfo = cursor.fetchone()
+
+        channel = freqToChannel(apinfo[3])
+
+        cursor.execute('SELECT MIN(time) FROM location WHERE apid=?', (apid,))
+        firsttime = cursor.fetchone()[0]
+
+        cursor.execute('SELECT MAX(time) FROM location WHERE apid=?', (apid,))
+        lasttime = cursor.fetchone()[0]
+
+        cursor.execute('SELECT MAX(level) FROM location WHERE apid=?', (apid,))
+        bestlevel = cursor.fetchone()[0]
+
+        cursor.execute('SELECT COUNT(*) FROM location WHERE apid=?', (apid,))
+        numberofloc = cursor.fetchone()[0]
+
+        conn.close()
+
+    elif app.config['CURRENT_DB_TYPE'] == 'mysql':
+        conn = pymysql.connect(host=app.config['CURRENT_DB_HOST'], user=app.config['CURRENT_DB_USER'],
+                               password=app.config['CURRENT_DB_PASS'], db=app.config['CURRENT_DB_NAME'])
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM ap WHERE id=%s', apid)
+        apinfo = cursor.fetchone()
+
+        channel = freqToChannel(apinfo[3])
+
+        cursor.execute('SELECT MIN(time) FROM location WHERE apid=%s', apid)
+        firsttime = cursor.fetchone()[0]
+
+        cursor.execute('SELECT MAX(time) FROM location WHERE apid=%s', apid)
+        lasttime = cursor.fetchone()[0]
+
+        cursor.execute('SELECT MAX(level) FROM location WHERE apid=%s', apid)
+        bestlevel = cursor.fetchone()[0]
+
+        cursor.execute('SELECT COUNT(*) FROM location WHERE apid=%s', apid)
+        numberofloc = cursor.fetchone()[0]
+        conn.close()
+
+    res = {"ssid": apinfo[2], "bssid": apinfo[1], "capabilities": apinfo[4], "channel": channel,
+           "vendor": apinfo[8], "bestlevel": bestlevel, "firsttime": firsttime, "lasttime": lasttime,
+           "numberofloc": numberofloc}
     return jsonify(res)
--- wifiapp/routes.py
+++ wifiapp/routes.py
@@ -1,6 +1,6 @@
 from wifiapp import app
 from flask import render_template, request
-from wifiapp.getdbinfo import apmarkers
+from wifiapp.getdbinfo import apmarkers, apinfo
 
 @app.route('/')
 @app.route('/index')
@@ -10,3 +10,7 @@ def index():
 @app.route('/getapmarkers', methods=['GET'])
 def getapmarkers():
     return apmarkers(request.args.get('bounds'))
+
+@app.route('/getapinfo', methods=['GET'])
+def getapinfo():
+    return apinfo(request.args.get('apid'))
